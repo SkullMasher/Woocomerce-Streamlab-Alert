12c254c4f1e7fa5128d177939d52fe71ec295c5c
Florian Ledru <florian@skullmasher.io>
2019-02-26 23:35:00 +0100

    :skull: Add an authorize link

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const json = {
+const merchAlert = {
   'name': 'Mister MV',
   'message': 'Incroyable du cul'
 }
@@ -9,7 +9,7 @@ const port = 3000
 
 app.get('/', (req, res) => {
   console.log('GET /')
-  res.send('hello world')
+  res.send('<a href="https://streamlabs.com/api/v1.0/authorize?response_type=code&client_id=AvS0kRZTc97kcUveOj1AzuhRSW1Z7vUGa8BLtDYy&redirect_uri=https%3A%2F%2Fskullmasher.io%2Fstreamlabs&scope=alerts.create">Cliquer ici</a> pour autoriser cette application à poster des alertes sur votre steam')
 })
 
 app.post('/', (req, res) => {
